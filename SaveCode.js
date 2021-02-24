77f80127a3d29dc2c2ce7e001c189ea12e5604bc
Tanish1 <34689436+Tanish1@users.noreply.github.com>
2021-02-24 18:49:35 -0500

    Forgot to push these...

--- SaveCode.js
+++ SaveCode.js
@@ -1,4 +1,4 @@
-function saveCode() {
+function saveCode() { //Doesn't add it to the directory, automatically saves upon key press
   var x = document.getElementById('codeInp'); 
   if (typeof(Storage) !== "undefined") {
     localStorage.setItem("save0", x.value);
@@ -7,7 +7,28 @@ function saveCode() {
     document.getElementById("result").innerHTML = "Your browser does not support web storage!";
   }
 }
-function getPreviousCode() {
+function getPreviousCode() { //Doesn't add it to the directory, runs upon the body loads
+  var x = document.getElementById('codeInp');
+  if (typeof(Storage) !== "undefined") {
+    x.value = localStorage.getItem("save0");
+  }
+  else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+
+
+function saveCodeWD() { //WD is "With Directory"
+  var x = document.getElementById('codeInp'); 
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("save0", x.value);
+    
+  } else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+
+function getPreviousCodeWD() { //WD is "With Directory"
   var x = document.getElementById('codeInp');
   if (typeof(Storage) !== "undefined") {
     x.value = localStorage.getItem("save0");
