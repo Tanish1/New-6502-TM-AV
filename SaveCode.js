05af866b9e4a6a575a96cc67ca0ebbce6e2c2d28
Tanish1 <34689436+Tanish1@users.noreply.github.com>
2021-03-16 11:42:33 -0400

    Coded Website.html, using that for now...

---
+++ SaveCode.js
@@ -0,0 +1,62 @@
+function saveCode() { //Doesn't add it to the directory, automatically saves upon key press
+  var x = document.getElementById('codeInp'); 
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("save0", x.value);
+    
+  } else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+function getPreviousCode() { //Doesn't add it to the directory, runs upon the body loads
+  var x = document.getElementById('codeInp');
+  if (typeof(Storage) !== "undefined") {
+    x.value = localStorage.getItem("save0");
+  }
+  else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+
+
+function saveCodeWD() { //WD is "With Directory"
+  var x = document.getElementById('codeInp'); 
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("save0", x.value);
+    
+  } else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+
+function getPreviousCodeWD() { //WD is "With Directory"
+  var x = document.getElementById('codeInp');
+  if (typeof(Storage) !== "undefined") {
+    x.value = localStorage.getItem("save0");
+  }
+  else {
+    document.getElementById("result").innerHTML = "Your browser does not support web storage!";
+  }
+}
+
+function download(){
+  var text = document.getElementById("codeInp").value;
+  text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+  var blob = new Blob([text], { type: "text/plain"});
+  var anchor = document.createElement("a");
+  anchor.download = "6502.txt";
+  anchor.href = window.URL.createObjectURL(blob);
+  anchor.target ="_blank";
+  anchor.style.display = "none"; // just to be safe!
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+}
+
+var realtimeOptions = {
+  clientId: '114704660813-2ghk0jsiaclc1a8h7ble4daag6g3ivus.apps.googleusercontent.com'
+}
+
+function startRealtime() {
+  var realtimeLoader = new rtclient.RealtimeLoader(realtimeOptions);
+  realtimeLoader.start();
+}
