371cf172bff9a84d7de3b9e0f8becff61b7839b6
Tanish1 <34689436+Tanish1@users.noreply.github.com>
2021-04-13 11:20:17 -0400

    Removed Archive

--- Archive/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'), path = require('path'), app = express();
-
-app.use(express.static(path.join(__dirname, 'Public')));
-app.get('/', function(req, res) {
-    res.redirect('index.html');
-});
-app.listen(process.env.PORT);
