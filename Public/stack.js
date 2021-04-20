d92c658f24fed6572c2571cfd3f057e26e069f2e
Tanish1 <34689436+Tanish1@users.noreply.github.com>
2021-04-20 11:40:58 -0400

    Updates

--- Public/stack.js
+++ Public/stack.js
@@ -1,5 +1,13 @@
 var stackList = [];
 
 function generateStack(stackList) {
-	
+	var i;
+	var hexString;
+	var zero = "00";
+	var final;
+	for (i = 1; i < 257; i++) {
+		hexString = i.toString(16);
+		final = hexString + ":" + zero;
+		stackList.push(final);
+	}
 } 
